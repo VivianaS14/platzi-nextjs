68468fa532fcfc890457ee0d422b66c4a7f13267
VivianaS14 <vasorianp@gmail.com>
2023-08-10 12:17:51 -0500

    feat: fetch to fluff by id

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,10 +1,29 @@
+"use client";
+
+import { TProduct } from "index";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Page({ params }) {
+  const fluffId = params.id;
+  const [product, setProduct] = useState<TProduct>();
+
+  useEffect(() => {
+    window
+      .fetch(`http://localhost:3000/api/fluffs/${fluffId}`)
+      .then((res) => res.json())
+      .then(({ data }) => setProduct(data));
+  }, []);
+
   return (
     <div>
-      <h3>Esta es la pagina de producto.</h3>
-      <p>My ID: {params.id}</p>
+      {product && (
+        <>
+          <h3>{product.name}</h3>
+          <p>{product.attributes.description}</p>
+          <img src={product.image as string} alt={product.name} />
+        </>
+      )}
       <hr />
 
       <button>
