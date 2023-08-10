98c0a471c7cb2b49826d07f17421518abed02bf5
VivianaS14 <vasorianp@gmail.com>
2023-08-10 11:55:52 -0500

    feat: fetch to all fluffs and render on main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,43 @@
+"use client";
+
 // Pagina estática porque no depende de un contenido externo
 // Este siempre sera el index de la app
+import { useEffect, useState } from "react";
 import NavBar from "../components/NavBar";
 import Link from "next/link";
+import { TProduct } from "index";
 
 export default function Page() {
+  const [productsList, setProductsList] = useState<TProduct[]>([]);
+
+  useEffect(() => {
+    window
+      .fetch("api/fluffs")
+      .then((response) => response.json())
+      .then(({ data }) => setProductsList(data));
+  }, []);
+
   return (
     <>
       <NavBar />
-      <h1>Hello, Next.js!</h1>
-      <p>It's a pleasure to meet you!</p>
-      <hr />
-      <div className="list-container">
-        <ul>
-          <li>
-            <Link href="/product/100">Product 100</Link>
+      <h1>Platzi Fluff!</h1>
+      <p>Should I eat a fluff today?</p>
+
+      <br />
+
+      <ul>
+        {productsList.map((fluff) => (
+          <li key={fluff.id}>
+            <h4>{fluff.name}</h4>
+            <img
+              src={fluff.image as string}
+              alt={`Fluff - ${fluff.name}`}
+              width={100}
+            />
+            <Link href={`/product/${fluff.id}`}>{fluff.sku}</Link>
           </li>
-          <li>
-            <Link href="/product/101">Product 101</Link>
-          </li>
-          <li>
-            <Link href="/product/102">Product 102</Link>
-          </li>
-          <li>
-            <Link href="/product/103">Product 103</Link>
-          </li>
-          <li>
-            <Link href="/product/104">Product 104</Link>
-          </li>
-        </ul>
-      </div>
+        ))}
+      </ul>
     </>
   );
 }
