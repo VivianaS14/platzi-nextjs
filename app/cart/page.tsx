8a827723fd1c5cfbb5824852500e888a37169bb5
VivianaS14 <vasorianp@gmail.com>
2023-08-13 18:21:30 -0500

    feat: added cart page

---
+++ app/cart/page.tsx
@@ -0,0 +1,13 @@
+export default function Page() {
+  return (
+    <div>
+      <h3>Hello from Cart</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam nobis
+        voluptatibus, saepe rerum aspernatur ab nihil beatae necessitatibus ut
+        laboriosam reiciendis cumque. Ducimus saepe asperiores cupiditate.
+        Temporibus perspiciatis hic ducimus.
+      </p>
+    </div>
+  );
+}
